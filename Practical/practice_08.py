426e6e661d9966ba8da202f336a4a8e673208faf
Rubaiya <rubaiyapathan@gmail.com>
2025-04-16 15:55:02 +0600

    your comment

---
+++ Practical/practice_08.py
@@ -0,0 +1,14 @@
+VC = input('Enter the letter : ')
+match VC:
+    case "a":
+        print(VC, 'is a vowel')
+    case "e":
+        print(VC, 'is a vowel')
+    case "i":
+        print(VC, 'is a vowel')
+    case "o":
+        print(VC,'is a vowel')
+    case "u" :
+        print(VC,'is a vowel')
+    case _:
+        print(VC,'is a consonant')
